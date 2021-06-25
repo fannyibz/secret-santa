7b15d3d0a5344243b5ecff38558b644137c87ae0
fannyibanez <fannyibanez89@gmail.com>
2021-06-25 12:05:10 +0200

    set-up: model participant added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_25_095944) do
+ActiveRecord::Schema.define(version: 2021_06_25_100424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2021_06_25_095944) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "participants", force: :cascade do |t|
+    t.string "first_name"
+    t.string "email"
+    t.bigint "event_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_participants_on_event_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +46,5 @@ ActiveRecord::Schema.define(version: 2021_06_25_095944) do
   end
 
   add_foreign_key "events", "users"
+  add_foreign_key "participants", "events"
 end
