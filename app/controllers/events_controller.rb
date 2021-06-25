e8e072ae95d93a8b4f6f88c4a018a4d7ebc89eda
fannyibanez <fannyibanez89@gmail.com>
2021-06-25 12:44:54 +0200

    set-up: event controller #new #create

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,17 @@
+class EventsController < ApplicationController
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+end
