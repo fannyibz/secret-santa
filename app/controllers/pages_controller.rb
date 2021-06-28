854230252f1a7abee24f02858e351eb4ef11be0f
fannyibanez <fannyibanez89@gmail.com>
2021-06-28 11:35:58 +0200

    last fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @alert = I18n.t('devise.failure.unauthenticated') if current_user.nil?
     @event = Event.new
   end
-  
 end
