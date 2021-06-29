1deb99fe939ab7a4301bd17ba59542d9a6e14569
fannyibanez <fannyibanez89@gmail.com>
2021-06-29 14:14:49 +0200

    bug: fixed

--- app/services/secret_santa_service.rb
+++ app/services/secret_santa_service.rb
@@ -9,11 +9,15 @@ class SecretSantaService
   def call
     @participants.each do |participant|
       # Reject owner id from @ids & Select an id item randomly
-      gift_participant_id = @ids.reject { | partcipant_id | partcipant_id == participant.id }.sample
+      if participant.id ==  @participants.first.id
+        gift_participant_id = @ids.last
+      else
+        gift_participant_id = @ids.reject { | participant_id | participant_id == participant.id }.sample
+      end
       # Add it to column
       participant.update(gift_participant: gift_participant_id)
       # Ensur nobody gone pich the same gift_partcipant
-      @ids.reject! { | partcipant_id | partcipant_id == gift_participant_id }
+      @ids.reject! { | participant_id | participant_id == gift_participant_id }
       # Send email to the participant with all infos
       participant.send_event_creation
     end
