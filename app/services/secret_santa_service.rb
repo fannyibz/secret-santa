7dc80e3fb1fddfe3f3d0af32db0a602531154fbf
Fanny Ibanez <fannyibanez89@gmail.com>
2021-06-30 16:26:01 +0200

    Bug last partcipant (#6)
    * bug: fixed
    
    * bug: adding comments

--- app/services/secret_santa_service.rb
+++ app/services/secret_santa_service.rb
@@ -8,12 +8,17 @@ class SecretSantaService
 
   def call
     @participants.each do |participant|
-      # Reject owner id from @ids & Select an id item randomly
-      gift_participant_id = @ids.reject { | partcipant_id | partcipant_id == participant.id }.sample
+      if participant.id ==  @participants.first.id
+        # Assign last partcipant to the the first one
+        gift_participant_id = @ids.last
+      else
+        # Reject owner id from @ids & Select an id item randomly
+        gift_participant_id = @ids.reject { | participant_id | participant_id == participant.id }.sample
+      end
       # Add it to column
       participant.update(gift_participant: gift_participant_id)
       # Ensur nobody gone pich the same gift_partcipant
-      @ids.reject! { | partcipant_id | partcipant_id == gift_participant_id }
+      @ids.reject! { | participant_id | participant_id == gift_participant_id }
       # Send email to the participant with all infos
       participant.send_event_creation
     end
